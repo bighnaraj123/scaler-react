b3b96fddf367608af3d079f9f42d11304424b2f5
bighnaraj123 <bighnaraj.upadhyaya@gmail.com>
2025-09-15 18:44:04 +0530

    idb clone

--- imdb-clone/src/helpers/gemini.js
+++ imdb-clone/src/helpers/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI (import.meta.env.VITE_GEMINI_API_KEY);
 
 export const getGeminiModel = async () => {
   return genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
